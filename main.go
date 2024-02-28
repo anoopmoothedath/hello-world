305fc9d5138276bc6559fe50dec881b49d26191e
Anoop M <anoop@digiledge.in>
2024-02-28 19:59:35 +0530

    changes made in feature branch

--- main.go
+++ main.go
@@ -5,4 +5,7 @@ import "log"
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	log.Println("Started Hello-World Program")
+
+	log.Println("Hello, World!")
+	log.Println("This line is being printed from feature branch")
 }
